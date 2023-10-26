1fefdb8f7d3d1405f1e27f259c563b5f225f6e9e
Alex <arthurfleck8@proton.me>
2023-10-26 17:19:59 +0200

    add fn to read a csv file

--- src/cljbox.clj
+++ src/cljbox.clj
@@ -1,6 +1,18 @@
-(ns cljbox)
+(ns cljbox
+  (:require
+   [clojure.data.csv :as csv]
+   [clojure.java.io :as io]))
 
-(defn add 
+(defn add
   "Adds two numbers and returns the result."
   [x y]
   (+ x y))
+
+(defn read-csv-file
+  "Reads the file at path and returns a lazy seq of vectors, each vector representing 
+   a row in the csv file. Includes the header row."
+  [path]
+  (with-open [reader (io/reader path)]
+    (doall
+     (csv/read-csv reader))))
+
